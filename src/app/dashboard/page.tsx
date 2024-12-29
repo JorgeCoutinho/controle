cd7a292f21a2d8fa06aef5498a86692477e56daa
JORGE MIKAEL COUTINHO ALVES <jmca.lic19@uea.edu.br>
2024-12-29 18:00:21 -0300

    configurando rotas privadas dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,16 @@
 import { Container } from "@/components/container";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import {  redirect } from 'next/navigation'
 
+export default async function Dashboard() {
+  const session = await getServerSession(authOptions);
 
-export default function Dashboard() {
+  if(!session || !session.user) {
+     redirect('/')
+  }
+  
+
   return (
     <Container>
         <h1>Página Dashboard</h1>
