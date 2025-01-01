c2c34f9526448cd04958e3247c1e8fa291a565cb
JORGE MIKAEL COUTINHO ALVES <jmca.lic19@uea.edu.br>
2025-01-01 12:13:27 -0300

    Criando rota api routes backend

---
+++ src/app/api/customer/route.ts
@@ -0,0 +1,37 @@
+
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+import prismaClient from "@/lib/prisma";
+
+export async function POST(req: Request) {
+    const session = await getServerSession(authOptions);
+
+    // Check if the user is authenticated
+    if (!session || !session.user) {
+        return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+
+    }
+
+
+    const { name, phone, email, address, userId } = await req.json();
+
+    try {
+        await prismaClient.customer.create({
+            data: {
+                name,
+                phone,
+                email,
+                address: address ? address : "",
+                userId: userId 
+            }
+        })
+        return NextResponse.json({ message: 'Cliente cadastrado com sucesso!' });
+    } catch (err) {
+        return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+    }
+
+
+
+    
+}
--- src/app/dashboard/customer/page.tsx
+++ src/app/dashboard/customer/page.tsx
@@ -16,14 +16,14 @@ export default async function Customer() {
             <main className="mt-9 mb-2">
                 <div className="flex items-center justify-between">
                     <h1 className="text-3xl font-bold">Meus CLientes</h1>
-                    <Link  href='/dashboard/customer/new' className="bg-blue-500 text-white px-4 py-1 rounded hover:scale-105 duration-300">
+                    <Link href='/dashboard/customer/new' className="bg-blue-500 text-white px-4 py-1 rounded hover:scale-105 duration-300">
                         Novo Cliente
                     </Link>
                 </div>
 
                 <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 mt-2" >
                     <CardCustomer />
-                    <CardCustomer />    
+                    <CardCustomer />
                     <CardCustomer />
                 </section>
             </main>
